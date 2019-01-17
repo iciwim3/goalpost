1cd085fc63b3761d641acc950186830bde6fc4ac
SKE <appybuildmore@gmail.com>
2019-01-16 19:18:57 -0800

    Built UI on SB, set constraints, added IBOutlet for tableview and IBAction for button.

--- goalpost-ske/Controller/GoalsVC.swift
+++ goalpost-ske/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost-ske
 //
 //  Created by Sain-R Edwards on 1/16/19.
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableview: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
+    
 }
 
