179743b6d0f7e6846d6b7b4860808365e311d5e5
SKE <appybuildmore@gmail.com>
2019-01-16 19:47:34 -0800

    Configured goal cell in SB, created goal cell class, and added functions to be able to update the cell.

---
+++ goalpost-ske/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-ske
+//
+//  Created by Sain-R Edwards on 1/16/19.
+//  Copyright © 2019 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+}
